c953ade08ed54dae544814f5beae067987951568
Artsiom Mezin <iketari@gmail.com>
2017-06-25 21:24:19 +0100

    babel polyfills for Symbol

--- karma.conf.js
+++ karma.conf.js
@@ -17,6 +17,7 @@ module.exports = function(config) {
 
     // list of files / patterns to load in the browser
     files: [
+      './node_modules/babel-polyfill/dist/polyfill.js',
       './framework/*.spec.js'
     ],
 
