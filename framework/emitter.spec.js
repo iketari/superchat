de3ffceda61aab4be1e909bce6002f114b544bc4
Artsiom Mezin <iketari@gmail.com>
2017-06-25 21:25:56 +0100

    Emitter spec

---
+++ framework/emitter.spec.js
@@ -0,0 +1,21 @@
+import {Emitter} from './emitter';
+
+describe('Emitter', () => {
+    let someObj, anotherObj;
+
+    beforeEach(() => {
+        someObj = {};
+    });
+
+    it('static method apply', () => {
+        Emitter.apply(someObj); 
+
+        
+        assert(someObj.hasOwnProperty('on'));
+    });
+
+
+
+   
+
+});
