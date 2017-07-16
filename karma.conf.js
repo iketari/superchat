c176b9ce81b1ba8696c0531e7574a3e3001261ed
Ostapenko Anatoliy <a.ostapenko@corp.mail.ru>
2017-07-16 14:19:40 +0300

    Настроил babel-preset-env для сборки (автопрефиксер для бабеля - уменьшает размер бандла и время сборки)

--- karma.conf.js
+++ karma.conf.js
@@ -3,116 +3,131 @@
 
 const path = require('path');
 
-module.exports = function(config) {
-  config.set({
-
-    // base path that will be used to resolve all patterns (eg. files, exclude)
-    basePath: '',
-
-
-    // frameworks to use
-    // available frameworks: https://npmjs.org/browse/keyword/karma-adapter
-    frameworks: ['mocha', 'chai', 'sinon'],
+module.exports = function (config) {
+	config.set({
+		// base path that will be used to resolve all patterns (eg. files, exclude)
+		basePath: '',
 
 
-    // list of files / patterns to load in the browser
-    files: [
-      './node_modules/babel-polyfill/dist/polyfill.js',
-      './src/**/*.spec.js'
-    ],
-
-    // list of files to exclude
-    exclude: [],
-
-    // preprocess matching files before serving them to the browser
-    // available preprocessors: https://npmjs.org/browse/keyword/karma-preprocessor
-    preprocessors: {
-      '**/*.spec.js': ['webpack']
-    },
-
-    // webpack configuration
-    webpack: {
-      devtool: 'inline-source-map',
-      module: {
-      rules: [
-          {
-            test: /\.js$/,
-            use: [{
-              loader: 'babel-loader',
-              options: { presets: ['es2015'] },
-            }],
-          },
-          // instrument only testing sources with Istanbul 
-          {
-            test: /\.js$/,
-            exclude: /(node_modules|\.spec\.js$)/,
-            loader: 'istanbul-instrumenter-loader',
-            enforce: 'post',
-            options: {
-              esModules: true
-            }
-          }
-        ]
-      }
-    },
-
-    webpackServer: {
-      noInfo: true //please don't spam the console when running in karma!
-    },
-
-    webpackMiddleware: {
-      stats: 'errors-only',
-      noInfo: true
-    },
-
-    // test results reporter to use
-    // possible values: 'dots', 'progress'
-    // available reporters: https://npmjs.org/browse/keyword/karma-reporter
-    reporters: [ 'progress', 'coverage-istanbul' ],
-
-    plugins: [
-      'karma-phantomjs-launcher',
-      'karma-coverage-istanbul-reporter',
-      'karma-mocha',
-      'karma-chai',
-      'karma-sinon',
-      'karma-webpack'
-    ],
-
-    // any of these options are valid: https://github.com/istanbuljs/istanbul-api/blob/47b7803fbf7ca2fb4e4a15f3813a8884891ba272/lib/config.js#L33-L38
-    coverageIstanbulReporter: {
-      reports: ['html'],
-      dir: path.join(__dirname, 'coverage', 'html')
-    },
-    // web server port
-    port: 9876,
+		// frameworks to use
+		// available frameworks: https://npmjs.org/browse/keyword/karma-adapter
+		frameworks: ['mocha', 'chai', 'sinon'],
 
 
-    // enable / disable colors in the output (reporters and logs)
-    colors: true,
+		// list of files / patterns to load in the browser
+		files: [
+			'./node_modules/babel-polyfill/dist/polyfill.js',
+			'./src/**/*.spec.js'
+		],
+
+		// list of files to exclude
+		exclude: [],
+
+		// preprocess matching files before serving them to the browser
+		// available preprocessors: https://npmjs.org/browse/keyword/karma-preprocessor
+		preprocessors: {
+			'**/*.spec.js': ['webpack']
+		},
+
+		// webpack configuration
+		webpack: {
+			devtool: 'inline-source-map',
+			module: {
+				rules: [
+					{
+						test: /\.js$/,
+						use: [{
+							loader: 'babel-loader',
+							options: {
+								babelrc: false,       // брать настройки из options
+								cacheDirectory: true, // включить кширование (node_modules/.cache/babel-loader)
+								presets: [
+									[
+										'env',
+										{
+											targets: {
+												browsers: ['last 2 versions'],
+												chrome: 49,
+												safari: 9,
+											},
+											debug: true
+										}
+									]
+								]
+							},
+						}],
+					},
+					// instrument only testing sources with Istanbul
+					{
+						test: /\.js$/,
+						exclude: /(node_modules|\.spec\.js$)/,
+						loader: 'istanbul-instrumenter-loader',
+						enforce: 'post',
+						options: {
+							esModules: true
+						}
+					}
+				]
+			}
+		},
+
+		webpackServer: {
+			noInfo: true //please don't spam the console when running in karma!
+		},
+
+		webpackMiddleware: {
+			stats: 'errors-only',
+			noInfo: true
+		},
+
+		// test results reporter to use
+		// possible values: 'dots', 'progress'
+		// available reporters: https://npmjs.org/browse/keyword/karma-reporter
+		reporters: ['progress', 'coverage-istanbul'],
+
+		plugins: [
+			'karma-phantomjs-launcher',
+			'karma-coverage-istanbul-reporter',
+			'karma-mocha',
+			'karma-chai',
+			'karma-sinon',
+			'karma-webpack'
+		],
+
+		// any of these options are valid: https://github.com/istanbuljs/istanbul-api/blob/47b7803fbf7ca2fb4e4a15f3813a8884891ba272/lib/config.js#L33-L38
+		coverageIstanbulReporter: {
+			reports: ['html'],
+			dir: path.join(__dirname, 'coverage', 'html')
+		},
+		// web server port
+		port: 9876,
 
 
-    // level of logging
-    // possible values: config.LOG_DISABLE || config.LOG_ERROR || config.LOG_WARN ||
-    // config.LOG_INFO || config.LOG_DEBUG
-    logLevel: config.LOG_INFO,
+		// enable / disable colors in the output (reporters and logs)
+		colors: true,
 
 
-    // enable / disable watching file and executing tests whenever any file changes
-    autoWatch: true,
+		// level of logging
+		// possible values: config.LOG_DISABLE || config.LOG_ERROR || config.LOG_WARN ||
+		// config.LOG_INFO || config.LOG_DEBUG
+		logLevel: config.LOG_INFO,
 
 
-    // start these browsers
-    // available browser launchers: https://npmjs.org/browse/keyword/karma-launcher
-    browsers: ['PhantomJS'],
+		// enable / disable watching file and executing tests whenever any file changes
+		autoWatch: true,
 
 
-    // Continuous Integration mode
-    // if true, Karma captures browsers, runs the tests and exits
-    singleRun: false,
+		// start these browsers
+		// available browser launchers: https://npmjs.org/browse/keyword/karma-launcher
+		browsers: ['PhantomJS'],
 
-    // Concurrency level
-    // how many browser should be started simultaneous
-    concurrency: Infinity
-  });
+
+		// Continuous Integration mode
+		// if true, Karma captures browsers, runs the tests and exits
+		singleRun: false,
+
+		// Concurrency level
+		// how many browser should be started simultaneous
+		concurrency: Infinity
+	});
 };
--- src/views/index.js
+++ src/views/index.js
@@ -1,3 +1,3 @@
-export { default as LoginView } from './login.view';
-export { default as ChatView } from './chat.view';
-export { default as MainView } from './main.view';
+export { default as Login } from './login.view';
+export { default as Chat } from './chat.view';
+export { default as Main } from './main.view';
