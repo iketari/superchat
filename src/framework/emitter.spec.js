894e49b452f28a78d78b0a6ecb096e190e92227d
Artsiom Mezin <iketari@gmail.com>
2017-07-19 23:23:42 +0100

    emmiter specs

--- src/framework/emitter.spec.js
+++ src/framework/emitter.spec.js
@@ -1,15 +1,37 @@
 import Emitter from './emitter';
 
 describe('Emitter', () => {
-	let someObj, anotherObj;
+	let someObj, anotherObj, callbacks;
 
 	beforeEach(() => {
 		someObj = {};
+
+		anotherObj = {};
+
+		callbacks = {
+			foo () {}
+		}
 	});
 
-	it('static method apply', () => {
-		Emitter.apply(someObj);
+	describe('apply', () => {
+		it('should extend object with methods on and trigger', () => {
+			Emitter.apply(someObj);
 
-		assert(someObj.hasOwnProperty('on'));
+			assert.property(someObj, 'on', 'The object should has own method on');
+			assert.property(someObj, 'trigger', 'The object should has own method on');
+		});
+
+		it('should not override existing events', () => {
+			let spy = sinon.spy(callbacks, 'foo');
+			Emitter.apply(someObj);
+
+			someObj.on('foo', callbacks.foo);
+
+			Emitter.apply(someObj);
+
+			someObj.trigger('foo')
+
+			assert(spy.called, 'The callback should be called');
+		});
 	});
 });
