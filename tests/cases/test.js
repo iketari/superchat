36bc93ea0b5543b689154578b56eaa80088b2008
Vladimir Demidov <vladimir.dem@gmail.com>
2018-02-27 23:28:18 +0300

    init webdriverio with chromedriver

---
+++ tests/cases/test.js
@@ -0,0 +1,8 @@
+const assert = require('assert');
+
+describe('Sign in button', () => {
+    it('should open sign in page', () => {
+        browser.url('https://components-e2e6e.firebaseapp.com/');
+        assert(true);
+    });
+});
