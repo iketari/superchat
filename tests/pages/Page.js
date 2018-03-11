02db02f755c9eae0a66540b9d1985c10b08ca313
Vladimir Demidov <v.demidov@corp.mail.ru>
2018-03-11 18:36:22 +0300

    wdio in travis

--- tests/pages/Page.js
+++ tests/pages/Page.js
@@ -1,58 +1,56 @@
-import { reverse } from "dns";
-
 class Page {
 
-    /**
+	/**
      * url for current page
      */
-    get path () {
-        throw new Error('No path. You should override this getter');
-    }
+	get path () {
+		throw new Error('No path. You should override this getter');
+	}
 
-    /**
+	/**
      * container for current page. It should be used in page's locators and in wait method
      */
-    get container() {
-        throw new Error('No container. You should override this getter');
-    }
+	get container() {
+		throw new Error('No container. You should override this getter');
+	}
 
-    /**
+	/**
      * locators
      */
-    get locators() {
-        return {
-            preloader: '#preloader-wrapper',
-        };
-    }
+	get locators() {
+		return {
+			preloader: '#preloader-wrapper',
+		};
+	}
 
-    /**
+	/**
      * Open page
      */
-    open() {
-        browser.url(this.path);
-    }
+	open() {
+		browser.url(this.path);
+	}
 
-    /**
+	/**
      * Wait for preloader visible
      * 
      * @param {boolean} reverse
      */
-    waitForPreloader(reverse) {
-        const value = reverse ? 0 : 1;
+	waitForPreloader(reverse) {
+		const value = reverse ? 0 : 1;
 
-        browser.waitUntil(() => {
-            return +browser.getCssProperty(this.locators.preloader, 'opacity').value === value;
-        });
-    }
+		browser.waitUntil(() => {
+			return +browser.getCssProperty(this.locators.preloader, 'opacity').value === value;
+		});
+	}
 
-    /**
+	/**
      * Wait for page visible
      */
-    wait() {
-        this.waitForPreloader(true);
+	wait() {
+		this.waitForPreloader(true);
 
-        browser.waitForVisible(this.container);
-    }
+		browser.waitForVisible(this.container);
+	}
 }
 
 export default Page;
